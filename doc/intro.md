369673414668af2ed093554df9e8a263c85f7cba
paulbowler <paul_bowler@yahoo.com>
2013-10-18 13:54:32 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to multi-queue
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/multi_queue/core_test.clj
@@ -0,0 +1,7 @@
+(ns multi-queue.core-test
+  (:require [clojure.test :refer :all]
+            [multi-queue.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
