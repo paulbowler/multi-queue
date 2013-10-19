009271e8c204ca8442e74f2b3d4ecdc7bf1a59d0
paulbowler <paul_bowler@yahoo.com>
2013-10-19 14:43:09 +0100

    Added new test case

--- src/multi_queue/core.clj
+++ src/multi_queue/core.clj
@@ -3,9 +3,10 @@
 (defn invert [coll k] (not (get coll k)))
 
 (def waiting-list [
+              {:name "James" :special false, :miles 0, :catchment true}
               {:name "Fred" :special false, :miles 10, :catchment false}
               {:name "Bob" :special false, :miles 50, :catchment false}
-              {:name "Jim" :special true, :miles 20, :catchment true}
+              {:name "Jim" :special true, :miles 20, :catchment false}
               {:name "Paul" :special true, :miles 1, :catchment true}
               {:name "Bert" :special false, :miles 5, :catchment true}
               ])
